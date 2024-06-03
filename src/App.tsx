7a19ba23878f0e7460a00eb3b05349e0d2927177
Orestis <orestisgamer23@gmail.com>
2024-06-03 10:53:15 +0300

    Added font to tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import CatCard from "./components/CatCard";
 
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+    <div className="min-h-screen flex items-center justify-center bg-gray-200 font-roboto ">
       <CatCard/>
     </div>
   );
